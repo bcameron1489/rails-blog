d4c0a77514fd90bcfa203011fac5552fa7289be6
bcameron1489 <bcameron1489@gmail.com>
2018-05-23 15:00:37 -0400

    Integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    BrianCameronViewTool::Renderer.copyright 'Brian Cameron', 'All rights reserved'
+  end
 end
