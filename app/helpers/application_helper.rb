9bc6b4b29ad2c2eb513fa350b97c5537d81353b7
bcameron1489 <bcameron1489@gmail.com>
2018-06-14 04:44:27 -0400

    Adjusted source helper method to display a more concise message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,7 @@ module ApplicationHelper
 
   def source_helper(layout_name)
     if session[:source]
-      greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
+      greeting = "Thanks for visiting me from #{session[:source]}. Welcome to my portfolio!"
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
